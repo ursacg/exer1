93058056b6b011f7a72dc968c268848c47b1d6fc
ursacg <130328005+ursacg@users.noreply.github.com>
2023-09-08 10:41:16 +0800

    Update add.js
    added divide function

--- add.js
+++ add.js
@@ -1,9 +1,18 @@
 function displaySum() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+    let firstNum = Number(document.getElementById('firstNum').innerHTML)
+    let secondNum = Number(document.getElementById('secondNum').innerHTML)
+    
+    let total = firstNum + secondNum;
+    document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  }
 
-  let total = firstNum + secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
-}
+  function displayQuotient(){
+    let firstDivideNum = Number(document.getElementById('firstNum').innerHTML)
+    let secondDivideNum = Number(document.getElementById('secondNum').innerHTML)
 
-document.getElementById('sumButton').addEventListener("click", displaySum);
+    let quotient = firstDivideNum / secondDivideNum;
+    document.getElementById("answerDivide").innerHTML = ` ${firstDivideNum} / ${secondDivideNum}, equals to ${quotient}`;
+  }
+
+  document.getElementById("sumButton").addEventListener("click", displaySum);
+  document.getElementById("divideButton").addEventListener("click", displayQuotient);
